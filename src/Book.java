84a84e2a590f4aa3f7380f3cde58ac47ea275df0
Aseem Jain <Aseem.Jain@rackspace.com>
2017-02-16 20:24:37 -0600

    added book domain object

---
+++ src/Book.java
@@ -0,0 +1,39 @@
+/**
+ * Created by asee2278 on 2/16/17.
+ */
+import java.util.*;
+
+public class Book {
+
+    int id, category;
+    String name, author;
+    Boolean isAssigned;
+
+    public Book(int id, String name, String author, Integer category, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.author = author;
+        this.isAssigned = false;
+    }
+
+    @Override
+    public int hashCode() {
+        return category;
+    }
+
+
+    public void feed(Map<Integer, List<Book>> simulatedHashMap) {
+        autoCreate(simulatedHashMap);
+        simulatedHashMap.get(this.hashCode()).add(this);
+    }
+
+
+    public void autoCreate(Map<Integer, List<Book>> simulatedHashMap){
+        if( simulatedHashMap.get(this.hashCode()) == null){
+            simulatedHashMap.put(this.hashCode(), new ArrayList<Book>());
+        }
+
+    }
+
+}
